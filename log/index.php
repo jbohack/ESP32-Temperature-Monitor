bae5d32e547141f13964ef6eb2b80fc162eba04a
Joe Bohack <joebohack@gmail.com>
2022-03-06 03:08:47 -0500

    Accept numbers or decimal only for requests.

--- log/index.php
+++ log/index.php
@@ -16,9 +16,15 @@ if(isset($_GET['temperature'])){
     $temperature = $_GET['temperature'];
     $measurement = 'F';
 
-    $stmt = $conn->prepare("INSERT INTO temperatureDataF (temperature, measurement) VALUES (?, ?)");
-    $stmt->bind_param("ss", $temperature, $measurement);
-    $stmt->execute(); 
+    // filter for numbers & decimal only
+    if (preg_match('/^[0-9]+(\\.[0-9]+)?$/', $temperature)){
+        debug($temperature. " is a temperature");
+        $stmt = $conn->prepare("INSERT INTO temperatureDataF (temperature, measurement) VALUES (?, ?)");
+        $stmt->bind_param("ss", $temperature, $measurement);
+        $stmt->execute(); 
+    } else {
+        debug($temperature. " is not a temperature");
+    }
 
     // close the connection
     $stmt->close();
