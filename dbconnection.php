b42415f84380b07d175896d3c460b8d102ce7d7f
Joe Bohack <joebohack@gmail.com>
2022-03-07 15:51:32 -0500

    Create SQL table structure if it doesn't already exist

--- dbconnection.php
+++ dbconnection.php
@@ -12,5 +12,17 @@ if ($conn->connect_error) {
     die("DB Connection failed: " . $conn->connect_error);
 }else{
     debug("DB Connection Successful");
+// Create table if it doesn't exist
+    $sqlStructure = "CREATE TABLE temperatureDataF (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        temperature varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+        measurement varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+        PRIMARY KEY (id))COLLATE='utf8_unicode_ci'";
+    if ($conn->query($sqlStructure) === TRUE) {
+        debug("Table structure created successfully");
+      } else {
+        debug("Table structure exists not creating");
+      }
 }
 ?>
