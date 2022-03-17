a2d5423631db37c23b20196d9f89105abcdd5365
Joe Bohack <joebohack@gmail.com>
2022-03-17 17:30:47 -0400

    Created hourly average, daily average, weekly average, and yearly average
    Created averages for hourly, daily, weekly, monthly, and yearly. This information is using SQL-derived tables to select the averages.

--- index.php
+++ index.php
@@ -42,20 +42,71 @@ if( $resultCurrentTemp->num_rows > 0 )
      $currentID = $row['id'];
 }
 
-// Select average temperature
-$averageTemp = "SELECT AVG(temperature) AS averageTemp FROM temperatureDataF";
-$resultAverageTemp = $conn->query($averageTemp);
-if( $resultAverageTemp->num_rows > 0 )
+// Select hourly average temperature
+$averageHourlyTemp = "SELECT AVG(hourlyAVG.temperature) AS hourlyAVG FROM (SELECT temperature FROM temperatureDataF ORDER BY id DESC LIMIT 12) hourlyAVG";
+$resultAverageHourlyTemp = $conn->query($averageHourlyTemp);
+if( $resultAverageHourlyTemp->num_rows > 0 )
 {
-     $row = $resultAverageTemp->fetch_assoc();
-     $averageTemperature = $row['averageTemp'];
+     $row = $resultAverageHourlyTemp->fetch_assoc();
+     $averageHourlyTemperature = $row['hourlyAVG'];
+}
+
+// Select daily average temperature
+$averageDailyTemp = "SELECT AVG(dailyAVG.temperature) AS dailyAVG FROM (SELECT temperature FROM temperatureDataF ORDER BY id DESC LIMIT 288) dailyAVG";
+$resultAverageDailyTemp = $conn->query($averageDailyTemp);
+if( $resultAverageDailyTemp->num_rows > 0 )
+{
+     $row = $resultAverageDailyTemp->fetch_assoc();
+     $averageDailyTemperature = $row['dailyAVG'];
+}
+
+// Select weekly average temperature
+$averageWeeklyTemp = "SELECT AVG(weeklyAVG.temperature) AS weeklyAVG FROM (SELECT temperature FROM temperatureDataF ORDER BY id DESC LIMIT 2016) weeklyAVG";
+$resultAverageWeeklyTemp = $conn->query($averageWeeklyTemp);
+if( $resultAverageWeeklyTemp->num_rows > 0 )
+{
+     $row = $resultAverageWeeklyTemp->fetch_assoc();
+     $averageWeeklyTemperature = $row['weeklyAVG'];
+}
+
+// Select monthly average temperature
+$averageMonthlyTemp = "SELECT AVG(monthlyAVG.temperature) AS monthlyAVG FROM (SELECT temperature FROM temperatureDataF ORDER BY id DESC LIMIT 8640) monthlyAVG";
+$resultAverageMonthlyTemp = $conn->query($averageMonthlyTemp);
+if( $resultAverageMonthlyTemp->num_rows > 0 )
+{
+     $row = $resultAverageMonthlyTemp->fetch_assoc();
+     $averageMonthlyTemperature = $row['monthlyAVG'];
+}
+
+// Select yearly average temperature
+$averageYearlyTemp = "SELECT AVG(yearlyAVG.temperature) AS yearlyAVG FROM (SELECT temperature FROM temperatureDataF ORDER BY id DESC LIMIT 105120) yearlyAVG";
+$resultAverageYearlyTemp = $conn->query($averageYearlyTemp);
+if( $resultAverageYearlyTemp->num_rows > 0 )
+{
+     $row = $resultAverageYearlyTemp->fetch_assoc();
+     $averageYearlyTemperature = $row['yearlyAVG'];
+}
+
+// Select total average temperature
+$averageTotalTemp = "SELECT AVG(temperature) AS totalAVG FROM temperatureDataF";
+$resultAverageTotalTemp = $conn->query($averageTotalTemp);
+if( $resultAverageTotalTemp->num_rows > 0 )
+{
+     $row = $resultAverageTotalTemp->fetch_assoc();
+     $averageTotalTemperature = $row['totalAVG'];
 }
 
 // display to the interface
 echo "<b>Maximum temperature:</b> ".number_format((float)$maximumTemperature, 2, '.', ''); echo " F";
 echo "<br><b>Minimum temperature:</b> ".number_format((float)$minimumTemperature, 2, '.', ''); echo " F";
-echo "<br><b>Average temperature:</b> ".number_format((float)$averageTemperature, 2, '.', ''); echo " F";
 echo "<br><b>Current temperature:</b> ".number_format((float)$currentTemperature, 2, '.', ''); echo " F";
+echo "<br><b>~~~~~~~~~~~~~~~~~~~~~~~~~~~~</b>";
+echo "<br><b>Hourly average:</b> ".number_format((float)$averageHourlyTemperature, 2, '.', ''); echo " F";
+echo "<br><b>Daily average:</b> ".number_format((float)$averageDailyTemperature, 2, '.', ''); echo " F";
+echo "<br><b>Weekly average:</b> ".number_format((float)$averageWeeklyTemperature, 2, '.', ''); echo " F";
+echo "<br><b>Monthly average:</b> ".number_format((float)$averageMonthlyTemperature, 2, '.', ''); echo " F";
+echo "<br><b>Yearly average:</b> ".number_format((float)$averageYearlyTemperature, 2, '.', ''); echo " F";
+echo "<br><b>All time average:</b> ".number_format((float)$averageTotalTemperature, 2, '.', ''); echo " F";
 echo "<br><b>~~~~~~~~~~~~~~~~~~~~~~~~~~~~</b>";
 echo "<br><b>Latest log time:</b> ".$currentTime. " UTC";
 echo "<br><b>Data size:</b> ".$currentID. " IDs";
